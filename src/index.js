83fdc7540d40f060aa581f9a2a4f0e4f6b9c1987
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-08 09:03:38 -0300

    feat: config the initial server configs

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+//const swaggerUI = require('swagger-ui-express');
+//const routes = require('./routes');
+//const errorHandler = require('./app/middlewares/errorHandler');
+//require('./infra/database');
+//const swaggerDocs = require('./swagger.json');
+
+class App {
+  constructor() {
+    this.express = express();
+    this.middlewares();
+    // this.routes();
+    // this.errors();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+    this.express.use(cors());
+    this.express.options('*', cors());
+  }
+
+  /*
+  routes() {
+    this.express.use(routes);
+  }
+  */
+
+  /*
+  errors() {
+    this.express.use(errorHandler);
+  }
+  */
+}
+
+module.exports = new App().express;

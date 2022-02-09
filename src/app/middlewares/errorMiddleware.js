04b88b894edbe856a4338c904c8c3f4375fd5712
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-09 09:02:17 -0300

    feat: create error middleware

---
+++ src/app/middlewares/errorMiddleware.js
@@ -0,0 +1,13 @@
+const NotFound = require('../errors/NotFound');
+const errorSerialize = require('../serialize/errorSerialize');
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = 500;
+
+  if (err instanceof NotFound) statusCode = 404;
+  else statusCode = 400;
+
+  res.status(statusCode).json(errorSerialize(err));
+};
+
+module.exports = errorHandler;

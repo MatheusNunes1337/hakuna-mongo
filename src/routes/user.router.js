6bced79ca3695fa8c4c8411ac4189243c64decf0
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 16:12:19 -0300

    feat: add updateUserValidation schema to patch endpoint of user.router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,11 +3,12 @@ const UserController = require('../app/controllers/UserController')
 const authentication = require('../app/middlewares/authenticationMiddleware')
 const createUserValidation = require('../app/validations/user/createUserSchema')
 const getAllUsersValidation = require('../app/validations/user/getAllSchema')
+const updateUserValidation = require('../app/validations/user/updateUserSchema')
 
 userRouter.get('/', authentication, getAllUsersValidation, UserController.findAll)
 userRouter.get('/:id', authentication, UserController.findById)
 userRouter.post('/', createUserValidation, UserController.create)
-userRouter.patch('/:id', authentication, UserController.update)
+userRouter.patch('/:id', authentication, updateUserValidation, UserController.update)
 userRouter.delete('/:id', authentication, UserController.delete)
 
 module.exports = userRouter

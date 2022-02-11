18a47d038b760515ab99b33f76c8bc679b096415
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 14:36:35 -0300

    feat: create AuthService with login method

---
+++ src/app/services/AuthService.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const bcrypt = require('bcryptjs');
+const UserRepository = require('../repositories/UserRepository');
+const InvalidCredentials = require('../errors/InvalidCredentials');
+
+dotenv.config();
+
+class AuthService {
+  async login(credentials) {
+    const { password } = credentials;
+    const user = await UserRepository.getByUsername(credentials.username);
+
+    if (!user) throw new InvalidCredentials('This username does not exist in database');
+
+    if (!(await bcrypt.compare(password, user.password)))
+      throw new InvalidCredentials('The password is incorrect. Try again');
+
+    const { _id } = user;
+    const token = jwt.sign({  _id: _id.toString() }, process.env.API_SECRET, {
+      expiresIn: '1d'
+    });
+
+    const response = { token, id: _id.toString() };
+
+    return response;
+  }
+}
+
+module.exports = new AuthService();

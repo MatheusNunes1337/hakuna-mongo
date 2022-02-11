240f2202e390c925baeff06c4b04a6e45cce72c5
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 15:23:38 -0300

    feat: create getAllUsesValidation schema

---
+++ src/app/validations/user/getAllSchema.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const getAllUsersValidation = async (req, res, next) => {
+ 
+    const schema = Joi.object({
+        offset: Joi.number().min(0),
+        limit: Joi.number().min(0),
+        username: Joi.string().trim().min(6),
+        email: Joi.string().email().trim(),
+        type: Joi.string().valid('student', 'teacher')
+    });
+
+    const { error } = await schema.validate(req.query, { abortEarly: false });
+    if (error) throw error;
+
+    return next();
+
+};
+
+module.exports = getAllUsersValidation;

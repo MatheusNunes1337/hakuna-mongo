c1b1760f8bb50980063ffadc696122e30e5c00a1
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 09:00:04 -0300

    feat: add user routes to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const userRouter = require('../routes/user.router')
 
 const routes = express.Router()
+routes.use('/api/v1/users', userRouter)
 
 module.exports = routes

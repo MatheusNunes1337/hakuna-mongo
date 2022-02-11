3c7cc1e974bcaa6b6488b6b871ce4aec79150acb
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 15:56:01 -0300

    feat: create createUserSchema validation

---
+++ src/app/validations/user/createUserSchema.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const createUserValidation = async (req, res, next) => {
+ 
+    const schema = Joi.object({
+        username: Joi.string().trim().min(6).required(),
+        email: Joi.string().email().trim().required(),
+        password: Joi.string().trim().min(6).required(),
+        type: Joi.string().valid('student', 'teacher').required(),
+        area: Joi.string().trim().min(3)
+    });
+
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+
+    return next();
+
+};
+
+module.exports = createUserValidation;

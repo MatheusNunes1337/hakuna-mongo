030752e5480cf96ac65ced58ef9a0247e8fb5b16
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 14:41:40 -0300

    feat: add authentication middleware to user routes except the post one

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,10 +1,11 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
+const authentication = require('../app/middlewares/authenticationMiddleware')
 
-userRouter.get('/', UserController.findAll)
-userRouter.get('/:id',UserController.findById)
+userRouter.get('/', authentication, UserController.findAll)
+userRouter.get('/:id', authentication, UserController.findById)
 userRouter.post('/', UserController.create)
-userRouter.patch('/:id', UserController.update)
-userRouter.delete('/:id', UserController.delete)
+userRouter.patch('/:id', authentication, UserController.update)
+userRouter.delete('/:id', authentication, UserController.delete)
 
 module.exports = userRouter

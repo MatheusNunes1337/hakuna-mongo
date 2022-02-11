9e65c221719c158dab56bfca7adf95b7656b3d99
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 15:25:29 -0300

    fix: remove errorSerialize import from loginSchema

--- src/app/validations/authentication/loginSchema.js
+++ src/app/validations/authentication/loginSchema.js
@@ -1,11 +1,10 @@
 const Joi = require('joi');
-const errorSerialize = require('../../serialize/errorSerialize');
 
 const loginValidation = async (req, res, next) => {
  
     const schema = Joi.object({
-      username: Joi.string().trim().min(6).required(),
-      password: Joi.string().trim().min(6).required()
+        username: Joi.string().trim().min(6).required(),
+        password: Joi.string().trim().min(6).required()
     });
 
     const { error } = await schema.validate(req.body, { abortEarly: false });

535bc0043ff7bc815d171bee70fa5576dd09b6d7
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 15:25:09 -0300

    feat: add getAllUsersValidation to getAllUsers endpoint in user.router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,9 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
 const authentication = require('../app/middlewares/authenticationMiddleware')
+const getAllUsersValidation = require('../app/validations/user/getAllSchema')
 
-userRouter.get('/', authentication, UserController.findAll)
+userRouter.get('/', authentication, getAllUsersValidation, UserController.findAll)
 userRouter.get('/:id', authentication, UserController.findById)
 userRouter.post('/', UserController.create)
 userRouter.patch('/:id', authentication, UserController.update)

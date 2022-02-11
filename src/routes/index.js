c6963182fe89dac41409e837866bb778e3215428
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 14:37:59 -0300

    feat: add auth route to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const userRouter = require('../routes/user.router')
+const userRouter = require('../routes/user.router');
+const authRouter = require('./auth.router');
 
 const routes = express.Router()
+
+routes.use('/api/v1/authentication', authRouter)
 routes.use('/api/v1/users', userRouter)
 
 module.exports = routes

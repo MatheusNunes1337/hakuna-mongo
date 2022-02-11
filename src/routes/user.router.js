86c31911aa79aede8de696c6048538cbd601a550
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 15:56:23 -0300

    feat: add createUserSchema validation to post endpoint in user.router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,11 +1,12 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
 const authentication = require('../app/middlewares/authenticationMiddleware')
+const createUserValidation = require('../app/validations/user/createUserSchema')
 const getAllUsersValidation = require('../app/validations/user/getAllSchema')
 
 userRouter.get('/', authentication, getAllUsersValidation, UserController.findAll)
 userRouter.get('/:id', authentication, UserController.findById)
-userRouter.post('/', UserController.create)
+userRouter.post('/', createUserValidation, UserController.create)
 userRouter.patch('/:id', authentication, UserController.update)
 userRouter.delete('/:id', authentication, UserController.delete)
 

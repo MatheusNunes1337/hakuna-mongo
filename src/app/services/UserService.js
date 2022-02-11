ff12e78bf0cfe78799c9746e62dd456b91a7fcef
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 09:26:11 -0300

    feat: implement create method in UserService

--- src/app/services/UserService.js
+++ src/app/services/UserService.js
@@ -1,5 +1,6 @@
 const userRepository = require('../repositories/UserRepository')
 const NotFound = require('../errors/NotFound')
+const Conflict = require('../errors/Conflict')
 
 class UserService {
     constructor() {
@@ -16,6 +17,21 @@ class UserService {
         if(!user) throw new NotFound('User')
 
         return user
+    }
+
+    async create(payload) {
+        let user
+        const {username, email} = payload
+        
+        user = await this.findAll({ username })
+    
+        if(user.docs.length > 0) throw new Conflict('This username is already in use')
+
+        user = await this.findAll({ email })
+
+        if(user.docs.length > 0) throw new Conflict('This email is already in use')
+
+        return this.userRepo.create(payload)
     }
 }
 

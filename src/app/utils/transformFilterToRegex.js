c35ee525aee484d37444d7c7e1507144f1c8e122
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-11 16:30:01 -0300

    feat: create transformFilterToRegex method

---
+++ src/app/utils/transformFilterToRegex.js
@@ -0,0 +1,11 @@
+const transformFilterToRegex = (filter) => {
+    const objFields = Object.keys(filter);
+    objFields.forEach((field) => {
+      filter[field] = new RegExp(filter[field], 'i');
+    });
+  
+    return filter;
+};
+
+module.exports = transformFilterToRegex
+  

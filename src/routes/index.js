0dd0374bb16960f8396a81e84fe285f1a630a3ce
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-14 09:16:42 -0300

    feat: add group routes to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const userRouter = require('../routes/user.router');
 const authRouter = require('./auth.router');
+const groupRouter = require('./group.router')
+
+const authentication = require('../app/middlewares/authenticationMiddleware')
 
 const routes = express.Router()
 
 routes.use('/api/v1/authentication', authRouter)
 routes.use('/api/v1/users', userRouter)
+
+routes.use(authentication)
+routes.use('/api/v1/groups', groupRouter)
 
 module.exports = routes

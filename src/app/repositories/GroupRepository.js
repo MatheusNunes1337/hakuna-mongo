a1ccf1a0c32c801fb3a4036fcf689f17bfe5c989
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-14 14:30:58 -0300

    feat: implement create method of GroupRepository

--- src/app/repositories/GroupRepository.js
+++ src/app/repositories/GroupRepository.js
@@ -5,6 +5,15 @@ class GroupRepository extends GenericRepository {
     constructor() {
         super(GroupSchema)
     }
+
+    async create(payload, userId) {
+        const group = await GroupSchema.create(payload)
+
+        group.members.push(userId)
+        group.mods.push(userId)
+
+        return group.save()
+    }
 }
 
 module.exports = new GroupRepository()

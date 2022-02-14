5eb4c90f55541b0600b0967e3c78ba728c64a56e
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-14 09:58:42 -0300

    feat: create findById method of GroupController

--- src/app/controllers/GroupController.js
+++ src/app/controllers/GroupController.js
@@ -5,6 +5,12 @@ class GroupController {
         const response = await GroupService.findAll(req.query)
         return res.status(200).json(response)
     }
+
+    async findById(req, res) {
+        const {id} = req.params
+        const response = await GroupService.findById(id)
+        return res.status(200).json(response)
+    }
 }
 
 module.exports = new GroupController()

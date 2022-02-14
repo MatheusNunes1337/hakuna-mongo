199a5032df4fa6d0c1212b59b197980261eebe4f
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-14 09:59:17 -0300

    feat: add endpoint to get a group by id

--- src/routes/group.router.js
+++ src/routes/group.router.js
@@ -2,5 +2,6 @@ const groupRouter = require('express').Router()
 const GroupController = require('../app/controllers/GroupController')
 
 groupRouter.get('/', GroupController.findAll)
+groupRouter.get('/:id', GroupController.findById)
 
 module.exports = groupRouter

834f938e6ec93f38cf40a01bc2bd4e7c47fce06f
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-18 11:43:08 -0300

    feat: add objectIdValidator to group and user endpoints

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,14 +1,16 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
 const authentication = require('../app/middlewares/authenticationMiddleware')
+
 const createUserValidation = require('../app/validations/user/createUserSchema')
 const getAllUsersValidation = require('../app/validations/user/getAllSchema')
 const updateUserValidation = require('../app/validations/user/updateUserSchema')
+const objectIdValidation = require('../app/validations/objectIdSchema')
 
 userRouter.get('/', authentication, getAllUsersValidation, UserController.findAll)
-userRouter.get('/:id', authentication, UserController.findById)
+userRouter.get('/:id', objectIdValidation, authentication, UserController.findById)
 userRouter.post('/', createUserValidation, UserController.create)
-userRouter.patch('/:id', authentication, updateUserValidation, UserController.update)
-userRouter.delete('/:id', authentication, UserController.delete)
+userRouter.patch('/:id', objectIdValidation, authentication, updateUserValidation, UserController.update)
+userRouter.delete('/:id', authentication, objectIdValidation, UserController.delete)
 
 module.exports = userRouter

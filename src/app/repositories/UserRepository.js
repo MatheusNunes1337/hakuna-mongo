89324987f3b59210246e0e3b748e16bc806b0de0
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-18 14:06:31 -0300

    feat: create delete method of UserRepository

--- src/app/repositories/UserRepository.js
+++ src/app/repositories/UserRepository.js
@@ -1,5 +1,6 @@
 const GenericRepository = require("./GenericRepository");
 const UserSchema = require('../schemas/UserSchema')
+const GroupSchema = require('../schemas/GroupSchema')
 
 class UserRepository extends GenericRepository {
     constructor() {
@@ -8,6 +9,18 @@ class UserRepository extends GenericRepository {
 
     async getByUsername(username) {
         return UserSchema.findOne({ username });
+    }
+
+    async delete(id) {
+        const { groups } = await UserSchema.findById(id)
+
+        await Promise.all(groups.map(async group => {
+            await GroupSchema.findByIdAndUpdate({_id: group}, 
+                {$pull: { members: id, mods: id }}
+            )
+        }))
+
+        return UserSchema.findByIdAndDelete(id)
     }
 }
 

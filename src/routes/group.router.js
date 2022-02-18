e7d1b534e4960a360819c10a71a3609fceecfe70
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-18 17:19:53 -0300

    feat: add modMiddleware to endpoints of group.router

--- src/routes/group.router.js
+++ src/routes/group.router.js
@@ -5,15 +5,16 @@ const createGroupValidation = require('../app/validations/group/createGroupSchem
 const updateGroupValidation = require('../app/validations/group/updateGroupSchema')
 const getAllGroupsValidation = require('../app/validations/group/getAllSchema')
 const objectIdValidation = require('../app/validations/objectIdSchema')
+const modMiddleware = require('../app/middlewares/group/modMiddleware')
 
 groupRouter.get('/', getAllGroupsValidation, GroupController.findAll)
 groupRouter.get('/:id', objectIdValidation, GroupController.findById)
 groupRouter.post('/', createGroupValidation, GroupController.create)
-groupRouter.patch('/:id', objectIdValidation, updateGroupValidation, GroupController.update)
+groupRouter.patch('/:id', objectIdValidation, modMiddleware, updateGroupValidation, GroupController.update)
 groupRouter.patch('/:id/join', objectIdValidation, GroupController.join)
-groupRouter.patch('/:id/mods/:userId', objectIdValidation, GroupController.addModerator)
-groupRouter.delete('/:id/mods/:userId', objectIdValidation, GroupController.removeModerator)
-groupRouter.delete('/:id/members/:userId', objectIdValidation, GroupController.removeMember)
-groupRouter.delete('/:id', objectIdValidation, GroupController.delete)
+groupRouter.patch('/:id/mods/:userId', objectIdValidation, modMiddleware, GroupController.addModerator)
+groupRouter.delete('/:id/mods/:userId', objectIdValidation, modMiddleware, GroupController.removeModerator)
+groupRouter.delete('/:id/members/:userId', objectIdValidation, modMiddleware, GroupController.removeMember)
+groupRouter.delete('/:id', objectIdValidation, modMiddleware, GroupController.delete)
 
 module.exports = groupRouter

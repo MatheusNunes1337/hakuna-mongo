6ff0dbbb103dcbc8537b04659a38702e2bbb8099
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-18 15:35:39 -0300

    feat: add recover routes to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const userRouter = require('../routes/user.router');
 const authRouter = require('./auth.router');
 const groupRouter = require('./group.router')
+const recoverRouter = require("./recover.router");
 
-const authentication = require('../app/middlewares/authenticationMiddleware')
+const authentication = require('../app/middlewares/authenticationMiddleware');
 
 const routes = express.Router()
 
 routes.use('/api/v1/authentication', authRouter)
 routes.use('/api/v1/users', userRouter)
+routes.use('/api/v1/recover', recoverRouter)
 
 routes.use(authentication)
 routes.use('/api/v1/groups', groupRouter)

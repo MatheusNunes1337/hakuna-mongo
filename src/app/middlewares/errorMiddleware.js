48351419fc3a5efd453236198041d914b42f2619
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-18 11:06:37 -0300

    feat: add updateGroupSchema validator to patch endpoint of group.router

--- src/app/middlewares/errorMiddleware.js
+++ src/app/middlewares/errorMiddleware.js
@@ -2,6 +2,7 @@ const NotFound = require('../errors/NotFound');
 const errorSerialize = require('../serialize/errorSerialize');
 
 const errorHandler = (err, req, res, next) => {
+  console.log(err)
   let statusCode = 500;
 
   if (err instanceof NotFound) statusCode = 404;
--- src/routes/group.router.js
+++ src/routes/group.router.js
@@ -2,11 +2,12 @@ const groupRouter = require('express').Router()
 const GroupController = require('../app/controllers/GroupController')
 
 const createGroupValidation = require('../app/validations/group/createGroupSchema')
+const updateGroupValidation = require('../app/validations/group/updateGroupSchema')
 
 groupRouter.get('/', GroupController.findAll)
 groupRouter.get('/:id', GroupController.findById)
 groupRouter.post('/', createGroupValidation, GroupController.create)
-groupRouter.patch('/:id', GroupController.update)
+groupRouter.patch('/:id', updateGroupValidation, GroupController.update)
 groupRouter.patch('/:id/join', GroupController.join)
 groupRouter.patch('/:id/mods/:userId', GroupController.addModerator)
 groupRouter.delete('/:id/mods/:userId', GroupController.removeModerator)

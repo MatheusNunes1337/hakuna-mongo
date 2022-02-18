e68903afa2565188e13ec860e59d407707c91c86
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-18 16:12:11 -0300

    add endpoint to recoverPassword in recover.router

--- src/routes/recover.router.js
+++ src/routes/recover.router.js
@@ -1,6 +1,8 @@
 const recoverRouter = require('express').Router()
 const MailController = require('../app/controllers/MailController')
+const RecoverController = require('../app/controllers/RecoverController')
 
 recoverRouter.post('/', MailController.sendMail)
+recoverRouter.patch('/:email', RecoverController.recoverPassword)
 
 module.exports = recoverRouter

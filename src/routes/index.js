4fd9f695ec48f7de5b9151f99cbc6081db2dce86
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-21 10:05:03 -0300

    feat: add post.router to routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ const userRouter = require('../routes/user.router');
 const authRouter = require('./auth.router');
 const groupRouter = require('./group.router')
 const recoverRouter = require("./recover.router");
+const postRouter = require('../routes/post.router')
 
 const authentication = require('../app/middlewares/authenticationMiddleware');
 
-const routes = express.Router()
+const routes = express.Router({mergeParams: true})
 
 routes.use('/api/v1/authentication', authRouter)
 routes.use('/api/v1/users', userRouter)
@@ -14,5 +15,6 @@ routes.use('/api/v1/recover', recoverRouter)
 
 routes.use(authentication)
 routes.use('/api/v1/groups', groupRouter)
+routes.use('/api/v1/groups/:groupId/posts', postRouter)
 
 module.exports = routes

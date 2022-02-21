f45ada9e3c8b4872982a835acdfe353fa22ab365
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-21 11:14:13 -0300

    add objectIdValidation to post router

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -1,7 +1,9 @@
 const postRouter = require('express').Router({mergeParams: true})
 const PostController = require('../app/controllers/PostController')
 const memberMiddleware = require('../app/middlewares/group/memberMiddleware')
+const objectIdValidation = require('../app/validations/objectIdSchema')
 
+postRouter.use(objectIdValidation)
 postRouter.use(memberMiddleware)
 postRouter.get('/', PostController.findAll)
 postRouter.get('/:id', PostController.findById)

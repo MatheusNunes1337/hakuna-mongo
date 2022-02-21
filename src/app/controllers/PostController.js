215e6180a0c846c6bfa8bcb1c1aa831f1a9c9e54
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-21 10:11:07 -0300

    feat: create findById method of PostController

--- src/app/controllers/PostController.js
+++ src/app/controllers/PostController.js
@@ -5,6 +5,12 @@ class PostController {
         const response = await PostService.findAll(req.query)
         return res.status(200).json(response)
     }
+
+    async findById(req, res) {
+        const { id } = req.params
+        const response = await PostService.findById(id)
+        return res.status(200).json(response)
+    }
 }
 
 module.exports = new PostController()

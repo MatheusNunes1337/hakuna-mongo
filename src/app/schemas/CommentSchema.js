997221a6796328882e6c039ff3112295d6869c11
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-21 16:03:37 -0300

    feat: create CommentSchema

---
+++ src/app/schemas/CommentSchema.js
@@ -0,0 +1,36 @@
+const { model, Schema } = require('mongoose');
+
+const CommentSchema = Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  creationDate: {
+    type: String,
+    required: true
+  },
+  creationTime: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  deslikes: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  },
+});
+
+const Comment = model('Comment', CommentSchema);
+
+module.exports = Comment;

cae1f5443390914709733cc3e3cb35f51432e9a1
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-21 10:48:20 -0300

    feat: implement create method of PostService

--- src/app/services/PostService.js
+++ src/app/services/PostService.js
@@ -1,5 +1,7 @@
 const NotFound = require('../errors/NotFound')
 const PostRepository = require('../repositories/PostRepository')
+const getCurrentDate = require('../utils/getCurrentDate')
+const getCurrentTime = require('../utils/getCurrentTime')
 const transformFilterToRegex = require('../utils/transformFilterToRegex')
 
 class PostService {
@@ -14,6 +16,14 @@ class PostService {
         if(!post) throw new NotFound('Post')
 
         return post
+    }
+
+    async create(payload, authorId) {
+        payload.creationDate = getCurrentDate()
+        payload.creationTime = getCurrentTime()
+        payload.author = authorId
+
+        return PostRepository.create(payload)
     }
 }
 

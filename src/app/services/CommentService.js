93d5dd175e2ae95ef0f67b618ea95b256e1a2a8a
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-22 09:54:36 -0300

    feat: create findById method of CommentService

--- src/app/services/CommentService.js
+++ src/app/services/CommentService.js
@@ -1,8 +1,16 @@
+const NotFound = require('../errors/NotFound')
 const CommentRepository = require('../repositories/CommentRepository')
 
 class CommentService {
     getAll(postId) {
         return CommentRepository.getAll(postId)
+    }
+
+    async findById({postId, id}) {
+        const comment = await CommentRepository.getById(id, postId)
+        if(!comment) throw new NotFound('Comment')
+        
+        return comment
     }
 }
 

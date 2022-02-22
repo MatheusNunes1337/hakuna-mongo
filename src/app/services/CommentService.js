9d3065c88d31a1b60d3c92b2ff273bba21f4be64
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-22 14:35:16 -0300

    feat: create update method of CommentService

--- src/app/services/CommentService.js
+++ src/app/services/CommentService.js
@@ -1,16 +1,32 @@
 const NotFound = require('../errors/NotFound')
 const CommentRepository = require('../repositories/CommentRepository')
+const getCurrentDate = require('../utils/getCurrentDate')
+const getCurrentTime = require('../utils/getCurrentTime')
 
 class CommentService {
     getAll(postId) {
         return CommentRepository.getAll(postId)
     }
 
-    async findById({postId, id}) {
+    async findById(postId, id) {
         const comment = await CommentRepository.getById(id, postId)
         if(!comment) throw new NotFound('Comment')
         
         return comment
+    }
+
+    async create(payload, postId, authorId) {
+        payload.creationDate = getCurrentDate()
+        payload.creationTime = getCurrentTime()
+        payload.author = authorId
+        payload.post = postId
+
+        return CommentRepository.create(payload, postId)
+    }
+
+    async update(payload, {id, postId}) {
+        await this.findById(postId, id)
+        return CommentRepository.update(id, payload)
     }
 }
 

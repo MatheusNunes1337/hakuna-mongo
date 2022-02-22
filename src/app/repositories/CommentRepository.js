6b3401ea4dc28b12184525d4afc1a5d12d996e8c
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-22 14:53:54 -0300

    feat: create delete method of CommentRepository

--- src/app/repositories/CommentRepository.js
+++ src/app/repositories/CommentRepository.js
@@ -1,5 +1,6 @@
 const GenericRepository = require("./GenericRepository");
-const CommentSchema = require('../schemas/CommentSchema')
+const CommentSchema = require('../schemas/CommentSchema');
+const PostSchema = require("../schemas/PostSchema");
 
 class CommentRepository extends GenericRepository {
     constructor() {
@@ -12,6 +13,20 @@ class CommentRepository extends GenericRepository {
 
     async getById(_id, post) {
         return await CommentSchema.findOne({_id, post})
+    }
+
+    async create(payload, postId) {
+        const post = await PostSchema.findById(postId)
+        const comment = await CommentSchema.create(payload);
+        post.comments.push(comment._id)
+        await post.save()
+
+        return comment
+    }
+
+    async delete(_id, post) {
+        await PostSchema.findByIdAndUpdate(post, {$pull: {comments: _id}})
+        return CommentSchema.findOneAndDelete({_id, post})
     }
 }
 

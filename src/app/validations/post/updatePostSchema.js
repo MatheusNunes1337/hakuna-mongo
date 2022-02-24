f9ea4a60a7c0db4f1a84011b09406b79890b3666
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-24 15:15:03 -0300

    feat: add likes and deslikes fields to updatePostSchema validator

--- src/app/validations/post/updatePostSchema.js
+++ src/app/validations/post/updatePostSchema.js
@@ -3,7 +3,9 @@ const Joi = require('joi');
 const updatePostValidation = async (req, res, next) => {
  
     const schema = Joi.object({
-        content: Joi.string().trim().max(2000)
+        content: Joi.string().trim().max(2000),
+        likes: Joi.string().valid('enable', 'disable'),
+        deslikes: Joi.string().valid('enable', 'disable'),
     });
 
     const { error } = await schema.validate(req.body, { abortEarly: false });

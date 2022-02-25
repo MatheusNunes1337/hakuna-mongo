661b82eb180a38ffe9c9dc6985f2b5a7744e2422
Matheus Nunes <matheus.silva@compasso.com.br>
2022-02-25 15:31:24 -0300

    feat: add multer middleware to patch endpoint of user.router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,6 +1,7 @@
 const userRouter = require('express').Router()
 const UserController = require('../app/controllers/UserController')
 const authentication = require('../app/middlewares/authenticationMiddleware')
+const imageUpload = require('../config/multer/imageUpload')
 
 const createUserValidation = require('../app/validations/user/createUserSchema')
 const getAllUsersValidation = require('../app/validations/user/getAllSchema')
@@ -10,7 +11,7 @@ const objectIdValidation = require('../app/validations/objectIdSchema')
 userRouter.get('/', authentication, getAllUsersValidation, UserController.findAll)
 userRouter.get('/:id', objectIdValidation, authentication, UserController.findById)
 userRouter.post('/', createUserValidation, UserController.create)
-userRouter.patch('/:id', objectIdValidation, authentication, updateUserValidation, UserController.update)
+userRouter.patch('/:id', objectIdValidation, authentication, updateUserValidation, imageUpload.single('profilePic'), UserController.update)
 userRouter.delete('/:id', authentication, objectIdValidation, UserController.delete)
 
 module.exports = userRouter

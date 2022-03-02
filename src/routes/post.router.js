8526fc19342c489c357fbffb238efc7816379df6
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-02 16:01:50 -0300

    feat: remove create and update post validations from post routes

--- src/routes/post.router.js
+++ src/routes/post.router.js
@@ -5,13 +5,14 @@ const objectIdValidation = require('../app/validations/objectIdSchema')
 const createPostValidation = require('../app/validations/post/createPostSchema')
 const updatePostValidation = require('../app/validations/post/updatePostSchema')
 const getAllPostsValidation = require('../app/validations/post/getAllSchema')
+const { fileUpload } = require('../config/multer/fileUpload')
 
 postRouter.use(objectIdValidation)
 postRouter.use(memberMiddleware)
 postRouter.get('/', getAllPostsValidation, PostController.findAll)
 postRouter.get('/:id', PostController.findById)
-postRouter.post('/', createPostValidation, PostController.create)
-postRouter.patch('/:id', updatePostValidation, PostController.update)
+postRouter.post('/', fileUpload.array('files', 3), PostController.create)
+postRouter.patch('/:id', PostController.update)
 postRouter.delete('/:id', PostController.delete)
 
 module.exports = postRouter

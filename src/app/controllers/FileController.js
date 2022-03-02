186d94c77b2a9ffcd6714f9b3a310962eaae5d41
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-02 11:04:57 -0300

    feat: create download method of FileController

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -5,6 +5,11 @@ class FileController {
         await FileService.delete(req.params)
         return res.status(204).end()
     }
+
+    async download(req, res) {
+        const file = await FileService.download(req.params)
+        return res.status(200).send(file)
+    }
 }
 
 module.exports = new FileController()

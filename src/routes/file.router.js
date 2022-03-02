1b77c4d64499751227725c3585482b110fad9058
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-02 11:05:15 -0300

    feat: add endpoint to download a file

--- src/routes/file.router.js
+++ src/routes/file.router.js
@@ -2,6 +2,7 @@ const FileController = require('../app/controllers/FileController')
 
 const fileRouter = require('express').Router()
 
+fileRouter.get('/download/:key', FileController.download)
 fileRouter.delete('/:key', FileController.delete)
 
 module.exports = fileRouter

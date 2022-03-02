920aa13dfca36c723710e863ff6c574653e9a4b3
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-02 14:53:59 -0300

    feat: add express.urlencoded method to middlewares in index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ class App {
 
   middlewares() {
     this.express.use(express.json());
+    this.express.use(express.urlencoded({ extended: true }));
     this.express.use(cors());
     this.express.options('*', cors());
   }

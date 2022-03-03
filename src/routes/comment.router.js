d1c8d667c18195b84468d98c3b41c18066d5341d
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-03 10:55:50 -0300

    feat: add multer middleware to post and patch endpoints of comment router

--- src/routes/comment.router.js
+++ src/routes/comment.router.js
@@ -5,13 +5,14 @@ const objectIdValidation = require('../app/validations/objectIdSchema')
 const createCommentValidation = require('../app/validations/comment/createCommentSchema')
 const updateCommentValidation = require('../app/validations/comment/updateCommentSchema')
 const getAllCommentsValidation = require('../app/validations/comment/getAllSchema')
+const { fileUpload } = require('../config/multer/fileUpload')
 
 commentRouter.use(objectIdValidation)
 commentRouter.use(memberMiddleware)
 commentRouter.get('/', getAllCommentsValidation, CommentController.findAll)
 commentRouter.get('/:id', CommentController.findById)
-commentRouter.post('/', CommentController.create)
-commentRouter.patch('/:id', CommentController.update)
+commentRouter.post('/', fileUpload.array('files', 3), CommentController.create)
+commentRouter.patch('/:id', fileUpload.array('files', 3), CommentController.update)
 commentRouter.delete('/:id', CommentController.delete)
 
 module.exports = commentRouter

cdfeb47942b4921159c0fe9b2ad9bcdf9bd56519
Matheus Nunes <matheus.silva@compasso.com.br>
2022-04-08 11:22:40 -0300

    feat: add maxMembers field to updateGroupSchema validation

--- src/app/validations/group/updateGroupSchema.js
+++ src/app/validations/group/updateGroupSchema.js
@@ -6,6 +6,7 @@ const updateGroupValidation = async (req, res, next) => {
         name: Joi.string().trim().max(25),
         description: Joi.string().max(100).trim(),
         discipline: Joi.string().trim(),
+        maxMembers: Joi.number().integer().max(500),
         topics: Joi.array().items(Joi.string().trim().required()).min(1).max(5).unique(),
         isPublic: Joi.boolean(),
         password: Joi.string().trim().min(6).allow(null)

e99272a51c761adda24350aa23224a829b98f65a
Matheus Nunes <matheus.silva@compasso.com.br>
2022-08-22 17:35:46 -0300

    feat: remove 6 caracteres restriction of get users

--- src/app/validations/user/getAllSchema.js
+++ src/app/validations/user/getAllSchema.js
@@ -5,7 +5,7 @@ const getAllUsersValidation = async (req, res, next) => {
     const schema = Joi.object({
         offset: Joi.number().min(0),
         limit: Joi.number().min(0),
-        username: Joi.string().trim().min(6),
+        username: Joi.string().trim().min(1),
         email: Joi.string().email().trim(),
         type: Joi.string().valid('student', 'teacher')
     });

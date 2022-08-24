3ad9f532fe4ecb22709fd2cfba355ad757b9da7d
Matheus Nunes <matheus.silva@compasso.com.br>
2022-08-24 16:06:56 -0300

    fix: fix mail service redirect url

--- src/app/services/MailService.js
+++ src/app/services/MailService.js
@@ -6,13 +6,13 @@ class MailService {
     async send({ receiverEmail }) {
         const user = await UserRepository.getByEmail(receiverEmail)
 
-        if(!user) throw new BadRequest('It seems there is not any user with this email on database')
+        if(!user) throw new BadRequest('Esse e-mail não existe na base de dados')
 
         return transporter.sendMail({
             from: `"${process.env.MAIL_SENDER_NAME}" ${process.env.MAIL_SENDER_EMAIL}`,
             to: receiverEmail,
             subject: "Recuperação da senha de acesso",
-			html: '<h1>Recuperação da senha de acesso</h1><p>Para recuperar a sua senha clique no link e você será redirecionado a uma página para redefinir a sua senha.</p><a href="http://hakunaaa.herokuapp.com/redefine-pass">redefinir senha<a/>'
+			html: '<h1>Recuperação da senha de acesso</h1><p>Para recuperar a sua senha clique no link e você será redirecionado a uma página para redefinir a sua senha.</p><a href="https://hakunaaa.herokuapp.com/redefine-pass">redefinir senha<a/>'
         })
     }
 }
